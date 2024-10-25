6daab1f760c82fff0ac1dda8ce586ec6faa30a0a
Entity 1011 <goffingslander@gmail.com>
2024-10-26 00:04:28 +0200

    update WaveManager.cs

--- gamejam24/Scripts/WaveManager.cs
+++ gamejam24/Scripts/WaveManager.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 public partial class WaveManager : Node
 {
@@ -8,9 +9,12 @@ public partial class WaveManager : Node
 	[Export]
 	public int Tokens;
 
+	private int _Tokens;
+
 	public override void _Ready()
 	{
-
+		// To make it immutable
+		_Tokens = Tokens;
 	}
 
 	public override void _Process(double delta)
