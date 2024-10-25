66f604144bee485cb59e8c218865bb27f083a53a
Entity 1011 <goffingslander@gmail.com>
2024-10-26 00:37:47 +0200

    update WaveManager.cs

--- gamejam24/Scripts/WaveManager.cs
+++ gamejam24/Scripts/WaveManager.cs
@@ -1,21 +1,27 @@
 using Godot;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Reflection.Metadata.Ecma335;
 
 public partial class WaveManager : Node
 {
 	[Export]
-	public Godot.GodotObject Asteroid;
+	public Godot.GodotObject[] Enemies = new GodotObject[0];
 	[Export]
 	public int Tokens;
+
+	[Export]
+	public int SpawnRadius;
 
 	private int _Tokens;
 
 	public override void _Ready()
 	{
+		GD.Print("Started");
 		// To make it immutable
 		_Tokens = Tokens;
-		
 	}
 
 	public override void _Process(double delta)
