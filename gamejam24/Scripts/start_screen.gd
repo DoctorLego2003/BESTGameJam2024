27f9e1e90af7497f9a4ce85f51d948269528a2f8
Arthur <arthur.vanbroeckhoven@student.kuleuven.be>
2024-10-26 00:04:38 +0200

    start screen/files

--- gamejam24/Scripts/start_screen.gd
+++ gamejam24/Scripts/start_screen.gd
@@ -1,10 +1,8 @@
 extends Control
 
-var counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Label.text = str(counter)
 	$Button.connect("pressed", Callable(self, "_on_button_pressed"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,9 +10,6 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-
 func _on_button_pressed() -> void:
-	if counter < 100:
-		counter += 1
-		$Label.text = str(counter)
+	
+	get_tree().change_scene_to_file("res://level.tscn")
