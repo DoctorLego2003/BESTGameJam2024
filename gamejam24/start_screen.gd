92a122ebf2cdd01079f31413ea7669bf3aade471
Arthur <arthur.vanbroeckhoven@student.kuleuven.be>
2024-10-25 23:47:38 +0200

    counter

--- gamejam24/start_screen.gd
+++ gamejam24/start_screen.gd
@@ -1,11 +1,20 @@
 extends Control
 
+var counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Label.text = str(counter)
+	$Button.connect("pressed", Callable(self, "_on_button_pressed"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+
+func _on_button_pressed() -> void:
+	if counter < 100:
+		counter += 1
+		$Label.text = str(counter)
