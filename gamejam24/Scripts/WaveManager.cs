fe2d8ff53aed35bc6ab81074bbf079c2ea664a3e
Entity 1011 <goffingslander@gmail.com>
2024-10-25 23:57:58 +0200

    update Main.tscn and Prefabs.cs

---
+++ gamejam24/Scripts/WaveManager.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class WaveManager : Node
+{
+	[Export]
+	public Godot.GodotObject Asteroid;
+
+	public override void _Ready()
+	{
+
+	}
+
+	public override void _Process(double delta)
+	{
+
+	}
+}
