a62d5c2605b743e3b6e8fe9e61d5f62e45bdf419
Pepijn Haenen <pepijn.haenen@gmail.com>
2024-10-26 14:41:22 +0200

    fixed shoot

--- gamejam24/Scripts/player_orbit.gd
+++ gamejam24/Scripts/player_orbit.gd
@@ -1,4 +1,8 @@
 extends Node2D
+var moveLeft = false
+var moveRight = false
+@export var speed = 3
+var projectile = load("res://projectile.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,10 +11,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if moveLeft:
+		self.rotate(speed * delta)
+	if moveRight:
+		self.rotate(-speed * delta)
 	
 func _input(event: InputEvent) -> void:
-	if event.is_action("clockwise-move"):
-		self.rotate(0.1)
-	elif event.is_action("anticlockwise-move"):
-		self.rotate(-0.1)
+	if event.is_action_pressed("clockwise-move"):
+		moveLeft = true
+	elif event.is_action_released("clockwise-move"):
+		moveLeft = false
+	if event.is_action_pressed("anticlockwise-move"):
+		moveRight = true
+	elif event.is_action_released("anticlockwise-move"):
+		moveRight = false
+	if event.is_action_pressed("shoot"):
+		get_parent().add_child(projectile.instantiate())
