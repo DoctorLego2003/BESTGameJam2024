09435126dd1f1ff4bb8d070df7bbbafcd9d4e7b4
Arthur <arthur.vanbroeckhoven@student.kuleuven.be>
2024-10-26 23:02:23 +0200

    cards are very nice

--- gamejam24/Scripts/card_generator.gd
+++ gamejam24/Scripts/card_generator.gd
@@ -12,55 +12,58 @@ var ry = y.randi_range(0, 2)
 var rz = z.randi_range(0, 2)
 
 func _process(delta: float) -> void:
-	pass
+	if not $Card1.visible and not $Card2.visible and not $Card3.visible:
+		get_tree().paused = false
+		get_node("Continue").visible = false
 
 func _ready() -> void:
 	# Ensure all images in the directory are loaded at startup
-	load_card_images()
+	#load_card_images()
+	get_tree().paused = true
+	get_node("Continue").visible = true
 	
 	
+
 
 func _on_card_1_pressed() -> void:
-	print(rx)
 	get_parent().get_node("ActivationFunctions").get_child(rx).activate()
 	$Card1.visible = false
+	
 
 func _on_card_2_pressed() -> void:
-	print(ry)
 	get_parent().get_node("ActivationFunctions").get_child(ry).activate()
 	$Card2.visible = false
 
 func _on_card_3_pressed() -> void:
-	print(rz)
 	get_parent().get_node("ActivationFunctions").get_child(rz).activate()
 	$Card3.visible = false
 	
 
 
-# Load all PNG images from the specified directory
-func load_card_images() -> void:
-	var dir = DirAccess.open(cards_directory)
-	if dir:
-		for file_name in dir.get_files():
-			if file_name.ends_with(".png"):
-				# Load the texture and add it to the card_images array
-				var texture = load(cards_directory + "/" + file_name)
-				card_images.append(texture)
-	else:
-		print("Error: Unable to open directory.")
-	
-	for i in range(1,4):
-		var rng = RandomNumberGenerator.new()
-		rng.randomize()
-		
-		# Check if card_images is not empty
-		if card_images.size() > 0:
-			var random_index = rng.randi_range(0, card_images.size() - 1)
-			var selected_card_texture = card_images[random_index]
-			
-			# Construct the dynamic node path and set the icon
-			var card_node = get_node("Card" + str(i))
-			if card_node:
-				card_node.icon = selected_card_texture
-			else:
-				print("Error: Node 'Card" + str(i) + "' not found in the scene tree.")
+## Load all PNG images from the specified directory
+#func load_card_images() -> void:
+	#var dir = DirAccess.open(cards_directory)
+	#if dir:
+		#for file_name in dir.get_files():
+			#if file_name.ends_with(".png"):
+				## Load the texture and add it to the card_images array
+				#var texture = load(cards_directory + "/" + file_name)
+				#card_images.append(texture)
+	#else:
+		#print("Error: Unable to open directory.")
+	#
+	#for i in range(1,4):
+		#var rng = RandomNumberGenerator.new()
+		#rng.randomize()
+		#
+		## Check if card_images is not empty
+		#if card_images.size() > 0:
+			#var random_index = rng.randi_range(0, card_images.size() - 1)
+			#var selected_card_texture = card_images[random_index]
+			#
+			## Construct the dynamic node path and set the icon
+			#var card_node = get_node("Card" + str(i))
+			#if card_node:
+				#card_node.icon = selected_card_texture
+			#else:
+				#print("Error: Node 'Card" + str(i) + "' not found in the scene tree.")
