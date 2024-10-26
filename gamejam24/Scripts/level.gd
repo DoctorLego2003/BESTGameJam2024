cb1681618b7ad29578bf0069eb24e5757aa6bfac
Arthur <arthur.vanbroeckhoven@student.kuleuven.be>
2024-10-26 11:42:28 +0200

    pause wekrtttt

--- gamejam24/Scripts/level.gd
+++ gamejam24/Scripts/level.gd
@@ -3,8 +3,14 @@ func _ready() -> void:
 	pass
 
 
-func _on_pausse_button_pressed() -> void:
+func _on_pause_button_pressed() -> void:
 	get_tree().paused = true
 	$PauseScreen.visible = true
 	$PauseButton.visible = false
 	
+
+#func _input(event):
+	#if event.is_action_pressed("Pause"):
+		#get_tree().paused = true
+		#$PauseScreen.visible = true
+		#$PauseButton.visible = false
--- gamejam24/Scripts/unpause.gd
+++ gamejam24/Scripts/unpause.gd
@@ -15,3 +15,14 @@ func _on_button_pressed() -> void:
 	get_tree().paused = false
 	self.visible = false
 	get_parent().get_node("PauseButton").visible = true
+
+#func _input(event):
+	#if event.is_action_pressed("Pause"):
+		#get_tree().paused = false
+		#self.visible = false
+		#get_parent().get_node("PauseButton").visible = true
+
+
+
+func _on_quit_game_pressed() -> void:
+	get_tree().quit()
