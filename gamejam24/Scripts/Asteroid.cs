5538af900e04de5d433d41d5d7ed2bcd55c82da3
Entity 1011 <goffingslander@gmail.com>
2024-10-26 18:49:37 +0200

    update Asteroid.cs

--- gamejam24/Scripts/Asteroid.cs
+++ gamejam24/Scripts/Asteroid.cs
@@ -7,8 +7,8 @@ public partial class Asteroid : Enemy
 
 	public override void _Ready()
 	{
-		int Angle = System.Security.Cryptography.RandomNumberGenerator.GetInt32(360);
-		int Distance = System.Security.Cryptography.RandomNumberGenerator.GetInt32(200) + 800;
+		float Angle = System.Security.Cryptography.RandomNumberGenerator.GetInt32(2*((int)(MathF.PI*100)))/100;
+		int Distance = System.Security.Cryptography.RandomNumberGenerator.GetInt32(1) + 200;
 		GlobalPosition = new Vector2(Distance * MathF.Cos(Angle), Distance * MathF.Sin(Angle));
 	}
 
@@ -18,10 +18,17 @@ public partial class Asteroid : Enemy
 	}
 	public new void Behavior(float Delta)
 	{
-		GlobalPosition = new Vector2
+		GlobalPosition += new Vector2
 		(
-			GlobalPosition.X + (Speed * MathF.Cos(MathF.Atan2(323, 577)) * Delta),
-			GlobalPosition.Y + (Speed * MathF.Sin(MathF.Atan2(323, 577)) * Delta)
+			Speed * MathF.Cos(MathF.Atan2(
+				new Vector2(323, 577).X - GlobalPosition.X,
+				new Vector2(323, 577).Y - GlobalPosition.Y
+				)) * Delta,
+			Speed * MathF.Sin(MathF.Atan2(
+				new Vector2(323, 577).X - GlobalPosition.X,
+				new Vector2(323, 577).Y - GlobalPosition.Y
+				)) * Delta
 		);
+		GD.Print(GlobalPosition);
 	}
 }
