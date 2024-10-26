573500888b7259c86cd718f5db87c099358ff656
Pepijn Haenen <pepijn.haenen@gmail.com>
2024-10-26 13:36:45 +0200

    Added projectile shooting

--- gamejam24/Scripts/planet.gd
+++ gamejam24/Scripts/planet.gd
@@ -1,5 +1,6 @@
 extends Area2D
 @onready var _animated_sprite = $MoonAnimation
+var projectile = load("res://projectile.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	_animated_sprite.play()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot"):
+		add_child(projectile.instantiate())
