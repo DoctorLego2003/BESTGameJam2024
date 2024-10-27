1dc3f6c618e48c5558f98c17802ef6280f9eb7be
Arthur <arthur.vanbroeckhoven@student.kuleuven.be>
2024-10-27 16:25:59 +0100

    .

--- gamejam24/Scripts/miner.gd
+++ gamejam24/Scripts/miner.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Area2D/AnimatedSprite2D.play("default")
+	get_tree().root.get_node("Level/WaveManager").WaveEnded.connect(self.give_money)
+
+func give_money() -> void:
+	get_tree().root.get_node("Level/ModManager").Money += 10
