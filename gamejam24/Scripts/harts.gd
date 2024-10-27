f286071b7a490ce1ce2d867421ddbf6e1cf62665
Arthur <arthur.vanbroeckhoven@student.kuleuven.be>
2024-10-27 15:57:05 +0100

    turrets cost money

--- gamejam24/Scripts/harts.gd
+++ gamejam24/Scripts/harts.gd
@@ -1,14 +1,14 @@
 extends TextureRect
-@onready var amount = get_tree().root.get_node("Level/ModManager").HealthMod
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hearts()
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	hearts()
+	
 
 func hearts() -> void:
-	get_node("Health").text = str(amount)
+	get_node("Health").text = str(get_tree().root.get_node("Level/ModManager").HealthMod)
